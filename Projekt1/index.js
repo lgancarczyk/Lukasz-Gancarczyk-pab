89266b7ddebbd8fefd85aa42b2e6e4a5b6970b91
lukasz.gancarczyk <lukasz.gancarczyk@PK6-17>
2022-03-09 20:01:37 +0100

    dodawanie

--- Projekt1/index.js
+++ Projekt1/index.js
@@ -4,3 +4,11 @@ app.get('/', function (req, res) {
     res.send('Hello World');
 });
 app.listen(3000);
+app.get('/:operation/:num1/:num2', function (req, res) {
+    var operation = req.params.operation;
+    var num1 = parseInt(req.params.num1);
+    var num2 = parseInt(req.params.num2);
+    if (operation == 'add') {
+        res.send((num1 + num2).toString());
+    }
+});
--- Projekt1/index.ts
+++ Projekt1/index.ts
@@ -5,3 +5,13 @@ app.get('/', function (req, res) {
 })  
 app.listen(3000) 
 
+app.get('/:operation/:num1/:num2', function (req, res) {
+    let operation = req.params.operation;
+    let num1 = parseInt(req.params.num1);
+    let num2 = parseInt(req.params.num2);
+    if(operation == 'add')
+    {
+        res.send((num1+num2).toString());
+    }
+    
+});
