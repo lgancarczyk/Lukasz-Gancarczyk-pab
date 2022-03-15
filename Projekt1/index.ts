23a6fd0e83f119cba5ee6f08ff44ce3852802d10
lgancarczyk <lukgan2000@gmail.com>
2022-03-15 09:23:24 +0100

    gotowe2

--- Projekt1/index.ts
+++ Projekt1/index.ts
@@ -5,13 +5,28 @@ app.get('/', function (req, res) {
 })  
 app.listen(3000) 
 
-app.get('/:operation/:num1/:num2', function (req, res) {
-    let operation = req.params.operation;
+app.get('/:operator/:num1/:num2', function (req, res) {
+    let operator = req.params.operator;
     let num1 = parseInt(req.params.num1);
     let num2 = parseInt(req.params.num2);
-    if(operation == 'add')
+    if(operator == 'add')
     {
         res.send((num1+num2).toString());
     }
-    
+    else if(operator =='substract')
+    {
+        res.send((num1-num2).toString());
+    }
+    else if(operator =='divide')
+    {
+        res.send((num1/num2).toString());
+    }
+    else if(operator =='multiply')
+    {
+        res.send((num1*num2).toString());
+    }
+    else
+    {
+      res.send('Wrong command. Commands(add, substract, divide, multiply/num1/num2)')
+    }
 });
