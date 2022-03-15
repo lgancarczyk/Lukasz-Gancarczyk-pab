79d495e0e59b1fe2d8ec007875c6ea038925790e
lgancarczyk <lukgan2000@gmail.com>
2022-03-15 09:22:14 +0100

    gotowe

--- Projekt1/index.js
+++ Projekt1/index.js
@@ -4,11 +4,23 @@ app.get('/', function (req, res) {
     res.send('Hello World');
 });
 app.listen(3000);
-app.get('/:operation/:num1/:num2', function (req, res) {
-    var operation = req.params.operation;
+app.get('/:operator/:num1/:num2', function (req, res) {
+    var operator = req.params.operator;
     var num1 = parseInt(req.params.num1);
     var num2 = parseInt(req.params.num2);
-    if (operation == 'add') {
+    if (operator == 'add') {
         res.send((num1 + num2).toString());
+    }
+    else if (operator == 'substract') {
+        res.send((num1 - num2).toString());
+    }
+    else if (operator == 'divide') {
+        res.send((num1 / num2).toString());
+    }
+    else if (operator == 'multiply') {
+        res.send((num1 * num2).toString());
+    }
+    else {
+        res.send('Wrong command. Commands(add, substract, divide, multiply/num1/num2)');
     }
 });
