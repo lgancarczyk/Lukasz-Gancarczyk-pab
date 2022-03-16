42e28a8e4a4e7eb26471ddcbbf6972a546b74402
lukasz.gancarczyk <lukasz.gancarczyk@PK6-17>
2022-03-16 20:01:19 +0100

    koniec zajec

--- Projekt2/src/index.ts
+++ Projekt2/src/index.ts
@@ -1,17 +1,26 @@
 //jeśli instaluje u siebie na omputerz, zainstaluj nodemon i ts-node globalnie (-g)
 import express from 'express'
 import {Request, Response} from 'express'
+import { json } from 'stream/consumers'
 
 const app = express()
+//const notes = []
 
 app.use(express.json())
 
 app.get('/', function (req: Request, res: Response) {
-  res.send('GET Hello World')
+  let note = {title:"test",content:"test_content"}
+  res.send(note)
 })
 app.post('/', function (req: Request, res: Response) {
   console.log(req.body) // e.x. req.body.title 
-  res.status(200).send('POST Hello World')
+
+  let title: string = req.body.title;
+  let content: string = req.body.content;
+  let note = {title:title,content:content}
+  res.send(note)
+
+  //res.status(200).send('POST Hello World')
 })
 
 app.listen(3000)
---
+++ Projekt2/src/note.ts
@@ -0,0 +1,4 @@
+interface Note{
+    title: string
+    content: string
+}
