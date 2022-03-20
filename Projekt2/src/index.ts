3036405c88fa0e52f9d1c4466c1460f625e177c1
lgancarczyk <lukgan2000@gmail.com>
2022-03-20 09:38:23 +0100

    gotowe

--- Projekt2/src/index.ts
+++ Projekt2/src/index.ts
@@ -13,22 +13,72 @@ app.use(express.json())
 app.get('/getall', function (req: Request, res: Response) {
   res.send(notes)
 })
+
 app.get('/note/:id', function (req: Request, res: Response) {
   let id  = req.params.id;
-  res.send(notes.filter(x =>x.id == +id))
+  let note = notes.filter(x =>x.id == +id)
+  if(notes.findIndex(x =>x.id == +id) == -1)
+  {
+    res.status(404).send("Note doesn`t exist!")
+  }
+  else
+  {
+    res.status(200).send(note);
+  }
 })
+
+app.put('/note/:id', function (req: Request, res: Response) {
+  let id  = req.params.id;
+  let note = notes.filter(x =>x.id == +id)
+  if(notes.findIndex(x =>x.id == +id) == -1)
+  {
+    res.status(404).send("Note doesn`t exist!")
+  }
+  else
+  {
+    if(req.body.title){
+      notes[notes.findIndex(x =>x.id == +id)].title = req.body.title
+    }
+    if(req.body.content){
+      notes[notes.findIndex(x =>x.id == +id)].content = req.body.content
+    }
+    res.status(204).send("Note updated.")
+  }
+})
+
 app.delete('/note/:id', function (req: Request, res: Response) {
   let id  = req.params.id;
-  notes.splice(notes.findIndex(x =>x.id == +id), 1)
   
-  res.send("Note deleted")
+  if(notes.findIndex(x =>x.id == +id) == -1)
+  {
+    res.status(404).send("Wrong Id!")
+  }
+  else
+  {
+    notes.splice(notes.findIndex(x =>x.id == +id), 1)
+    res.status(204).send("Note deleted.")
+  }
 })
-app.post('/note', function (req: Request, res: Response) {
-  console.log(req.body) 
 
-  let note: Note = new Note(req.body.title, req.body.content);
-  notes.push(note);
-  res.send(note)
+app.post('/note', function (req: Request, res: Response) {
+  
+  if(req.body.title === "" || req.body.content === "")
+  {
+    res.status(400).send("Title and Content can not be empty!")
+  }
+  else if(!req.body.title || !req.body.content)
+  {
+    res.status(400).send("Wrong arguments!")
+  }
+  else
+  {
+    console.log(req.body) 
+
+    let note: Note = new Note(req.body.title, req.body.content);
+    notes.push(note);
+    res.status(201).send(note.id.toString());
+  }
+  
 })
 
 app.listen(3000)
--- Projekt2/src/note.ts
+++ Projekt2/src/note.ts
@@ -1,4 +1,14 @@
-interface Note{
-    title: string
-    content: string
-}
+import internal from "stream";
+
+export class Note{
+    title: string;
+    content: string;
+    id: number;
+    constructor(title: string, content: string)
+    {
+        this.title = title;
+        this.content = content;
+        this.id = Date.now()
+        console.log("Note created");
+    }
+  }
