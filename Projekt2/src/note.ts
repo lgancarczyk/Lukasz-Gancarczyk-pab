f1314ddbc3c320a79156742398a98283cf9273a8
lgancarczyk <lukgan2000@gmail.com>
2022-03-31 13:06:31 +0200

    get/note/id

--- Projekt2/src/note.ts
+++ Projekt2/src/note.ts
@@ -25,11 +25,25 @@ export class Note{
         notes.push(note)
         fs.writeFileSync('Notes.json', JSON.stringify(notes))
     }
+
+    public IsInDatabase(id:number):boolean
+    {
+        let notes = this.ReadAllFileToJSON()
+        const elemFound = notes.some(el => el.id === id);
+        return elemFound
+
+    }
+    public GetNote(id:number)
+    {
+        let notes = this.ReadAllFileToJSON()
+        //let notesWithTags: Array<NoteViewModel> = new Array()
+        const NotesElementId = notes.findIndex(el => el.id === id);
+        let noteWithTags: NoteViewModel = new NoteViewModel(notes[NotesElementId])
+        return noteWithTags
+    }
     public GetAllNote()
     {
-        let rawdata = fs.readFileSync('Notes.json')
-        let notes: Note[] = JSON.parse(rawdata.toString())
-        //let notesWithTags: NoteViewModel[]
+        let notes = this.ReadAllFileToJSON()
         let notesWithTags: Array<NoteViewModel> = new Array()
         notes.forEach(element => {
             let noteModel:NoteViewModel = new NoteViewModel(element)
