526ecf91da6047653cfe1a8da66e4ba81d343a4f
lukasz.gancarczyk <lukasz.gancarczyk@PK6-17>
2022-04-20 19:59:41 +0200

    zajecia

--- Projekt2_1/src/index.ts
+++ Projekt2_1/src/index.ts
@@ -1,4 +1,5 @@
 import {Request, Response} from 'express'
+import {main} from './services/dbcontext'
 const express = require('express')  
 const app = express()
 const notes = require('./controllers/notesController')
@@ -6,7 +7,7 @@ const users = require('./controllers/usersController')
 
 app.use(express.json())
 
-
+main()
 app.use('/notes', notes)
 app.use('/users', users)
 
