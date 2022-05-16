8ffe74d08be0c861c4481d984f760d0aab2b0b3b
lukasz.gancarczyk <lukasz.gancarczyk@idealogic.pl>
2022-05-16 18:02:50 +0200

    deletin rates

--- Strona_z_przepisami/src/dbSchemas/recipeSchema.ts
+++ Strona_z_przepisami/src/dbSchemas/recipeSchema.ts
@@ -5,6 +5,11 @@ var rateSchema = new mongoose.Schema({
     RateUserId: {type: mongoose.Schema.Types.ObjectId, ref: 'user'}
 }, { _id : false });
 
+// var commentSchema = new mongoose.Schema({
+//     Rate: {type:Number, min:[1, 'min rate is 1'], max:[5, 'max rate is 5']},
+//     RateUserId: {type: mongoose.Schema.Types.ObjectId, ref: 'user'}
+// }, { _id : false });
+
 const recipeSchema = new mongoose.Schema({
     //ReicpeId: { type:String, required:true, unique:true, index:true, default:mongoose.Types.ObjectId },
     UserId: {type: mongoose.Schema.Types.ObjectId, ref: 'user'},
